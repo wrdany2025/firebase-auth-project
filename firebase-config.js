c60977bb1dfe7368ba132b2a165c689f1ad29546
wrdany2025 <mohmed.686.wrdany@gmail.com>
2025-07-05 19:02:11 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDmBogG3TLuqvGHIpdVQbxaxFqo9iDxh30",
+  authDomain: "online-store-a49f6.firebaseapp.com",
+  projectId: "online-store-a49f6",
+  storageBucket: "online-store-a49f6.appspot.com",
+  messagingSenderId: "787111961400",
+  appId: "1:787111961400:web:dd1de31f623d8ef44457cd"
+};
+
+firebase.initializeApp(firebaseConfig);
